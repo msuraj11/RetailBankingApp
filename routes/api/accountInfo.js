83293a04ed8910a49686b24708271cd0bb42667e
msuraj11 <suraj.mamidi@gmail.com>
2020-06-07 23:40:56 +0530

    New api under accountInfo for statements

--- routes/api/accountInfo.js
+++ routes/api/accountInfo.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const moment = require('moment');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const Transactions = require('../../models/Transactions');
 const AccountInfo = require('../../models/AccountInfo');
 
 const router = express.Router();
+
+// @route   GET api/accountInfo/statement/:start_date/:end_date
+// @desc    To get account statement from day to day
+// @access  Private
+router.get('/statement/:start_date/:end_date', auth, async (req, res) => {
+    try {
+        const transactions = await Transactions.findOne({ user: req.user.id });
+        const {txDetails} = transactions;
+        const {start_date, end_date} = req.params;
+
+        if (!(moment(start_date).isValid() && moment(end_date).isValid())) {
+            return res.status(400).json({ msg: 'Please provide valid dates' });
+        }
+
+        const filteredTxDetails = txDetails.filter(item => {
+            return moment(item.txDates).isBetween(start_date, end_date);
+        });
+        if (filteredTxDetails.length === 0) {
+            return res.status(400).json({ msg: 'There are no transsactions in these dates' });
+        }
+        return res.json(filteredTxDetails);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 // @route   GET api/accountInfo
 // @desc    This is to get the account Info
@@ -40,7 +68,7 @@ router.get('/', auth, async (req, res) => {
             });
     
             await accountInformation.save();
-            res.json(accountInformation);
+            return res.json(accountInformation);
         }
         
         // Already fetched/ updated
@@ -51,7 +79,7 @@ router.get('/', auth, async (req, res) => {
             );
         
         await isAccountInfoUpdated.save();
-        res.json(isAccountInfoUpdated);
+        return res.json(isAccountInfoUpdated);
 
     } catch (err) {
         console.log(err.message);
