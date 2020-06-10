46ad8715fd2d4f93c1f18304857381756b555b74
msuraj11 <suraj.mamidi@gmail.com>
2020-06-11 01:44:41 +0530

    Deleted unnecessary schema: accountinfo and modified accountInfo route

--- routes/api/accountInfo.js
+++ routes/api/accountInfo.js
@@ -3,7 +3,6 @@ const moment = require('moment');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const Transactions = require('../../models/Transactions');
-const AccountInfo = require('../../models/AccountInfo');
 
 const router = express.Router();
 
@@ -48,38 +47,19 @@ router.get('/', auth, async (req, res) => {
             .json({ msg: 'There is no profile for this user. Please do KYC to get Account Information' });
         }
 
-        const {accountNumber: accNo, accountType: accType, firstName, lastName,
-            IFSC_Code: IFSC, accBranch: branch} = profile;
-        const {accountBalance: totalAccBalance} = transactions;
+        const {accountNumber, accountType, firstName, lastName,
+            IFSC_Code, accBranch} = profile;
+        const {accountBalance} = transactions;
 
-        // Already fetched this API anytime before? then just update the accountBalance
-        // If it's not fetched then creating new instance in data-base
-        let isAccountInfoUpdated = await AccountInfo.findOne({ user: req.user.id });
-        // First fetch
-        if (!isAccountInfoUpdated) {
-            const accountInformation = new AccountInfo({
-                user: req.user.id,
-                totalAccBalance: transactions ? totalAccBalance : 0,
-                accNo,
-                accType,
-                accHolder: `${firstName} ${lastName}`,
-                IFSC,
-                branch
-            });
-    
-            await accountInformation.save();
-            return res.json(accountInformation);
+        const accountInfo = {
+            accountNumber,
+            accountType,
+            accountBalance: transactions ? accountBalance : 0,
+            accHolder: `${firstName} ${lastName}`,
+            IFSC_Code,
+            accBranch
         }
-        
-        // Already fetched/ updated
-        isAccountInfoUpdated = await AccountInfo.findOneAndUpdate(
-                {user: req.user.id},
-                {totalAccBalance},
-                {new: true}
-            );
-        
-        await isAccountInfoUpdated.save();
-        return res.json(isAccountInfoUpdated);
+        res.json(accountInfo);
 
     } catch (err) {
         console.log(err.message);
